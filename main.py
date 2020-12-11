130eb0813c33ce5e14a9a6536218d6db3aef55f6
Segroeg21 <63827364+Segroeg21@users.noreply.github.com>
2020-12-11 13:01:37 -0700

    put an item class
    dunno if it works since my code has weird random errors

--- main.py
+++ main.py
@@ -43,103 +43,22 @@ WHITE = (255, 255, 255)
 DARKGREY = (40, 40, 40)
 BGCOLOR = DARKGREY
 
-class Inventory():
-    def __init__(self, player, totalSlots, cols, rows):
-        self.player = player
-        self.totalSlots = totalSlots
-        self.cols = cols
-        self.rows = rows
-        self.appendSlots()
-        self.inventory_slots = []
-        self.display_inventory = False
+class Item():
+    def __init__(self):
+        self.items = []
 
-        self.movingitem = None
-        self.movingitemslot = None
+    def pick_up(self):
+        if len(item) >= 2:
+            print('Your inventory is full, cannot pick up item')
+        else:
+            self.items.append()
+            print('You picked up an item')
 
-    def appendSlots(self):
-            while len(self.inventory_slots) != self.totalSlots:
-                for x in range(WIDTH//2 - ((INVTILESIZE+2) * self.cols)//2, WIDTH//2 + ((INVTILESIZE+2) * self.cols) //2, INVTILESIZE+2):
-                    for y in range(UIHEIGHT, UIHEIGHT+INVTILESIZE * self.rows, INVTILESIZE+2):
-                        self.inventory_slots.append(InventorySlot(x, y))
+    def add_item(self, item):
+        self.items.append(item)
 
-    def draw(self, screen):
-        if self.display_inventory:
-            for slot in self.inventory_slots:
-                slot.draw(screen)
-            for slot in self.inventory_slots:
-                slot.drawItems(screen)
-
-    def toggleInventory(self):
-	    	self.display_inventory = not self.display_inventory
-
-    def addItemInv(self, item, slot=None):
-            if slot == None:
-                for slots in self.inventory_slots:
-                    if slots.item == None:
-                        slots.item = item
-                        break
-            if slot != None:
-                if slot.item != None:
-                    self.movingitemslot.item = slot.item
-                    slot.item = item
-                else:
-                    slot.item = item
-            
-    def removeItemInv(self, item):
-            for slot in self.inventory_slots:
-                if slot.item == item:
-                    slot.item = None
-                    break
-    
-    def moveItems(self, screen):
-        mousepos = pg.mouse.get_pos()
-        for slot in self.inventory_slots:
-		        if slot.draw(screen).collidepoint(mousepos) and slot.item != None:
-                            slot.item.is_moving = True
-                            self.movingitem = slot.item
-                            self.movingitemslot = slot
-                            break
-
-    def placeItem(self, screen):
-            mousepos = pg.mouse.get_pos()
-            for slot in self.inventory_slots:
-                    if slot.draw(screen).collidepoint(mousepos) and self.movingitem != None:
-                            if isinstance(slot, InventorySlot) and not isinstance(slot) and not isinstance(self.movingitemslot):
-                                    self.removeItemInv(self.movingitem)
-                                    self.addItemInv(self.movingitem, slot)
-                                    break
-            if self.movingitem != None:
-                    self.movingitem.is_moving = False
-                    self.movingitem = None
-                    self.movingitemslot = None
-    
-    def useItem(self, item):
-            if isinstance(item,):
-                item.use(self, self.player)
-
-class InventorySlot:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-		self.item = None
-
-	def draw(self, screen):
-		return pg.draw.rect(screen, WHITE, (self.x, self.y, INVTILESIZE, INVTILESIZE))
-	
-	def drawItems(self, screen):
-		if self.item != None and not self.item.is_moving:
-			self.image = pg.image.load(self.item.img).convert_alpha()
-			screen.blit(self.image, (self.x-7, self.y-7))
-		if self.item != None and self.item.is_moving:
-			mousepos1 = pg.mouse.get_pos()
-			self.image = pg.image.load(self.item.img).convert_alpha()
-			screen.blit(self.image, (mousepos1[0]-20,mousepos1[1]-20))    
-    
-class InventoryItem:
-	def __init__(self, img, value):
-		self.img = img
-		self.value = value
-		self.is_moving = False
+item = Item()
+item.add_item('Key')
 
 class Scrolling():
     def __init__(self, player):
